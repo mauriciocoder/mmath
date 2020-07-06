ebcb1948330a3f1405c57e31f6e7392ffb57def2
Mauricio <mauricio.bonetti@tailwindsw.com>
2020-07-06 14:44:07 -0300

    z-score, standard deviation, median, etc

--- mmath/statistics.py
+++ mmath/statistics.py
@@ -5,7 +5,7 @@
 # Released under GNU Public License (GPL)
 # email: mauricio.coder@outlook.com
 # -----------------------------------------------------------
-# TODO: iqr/ variance/ standard deviation
+import math  
 from functools import reduce
 
 def assertList(values):
@@ -21,8 +21,8 @@ def mean(values):
 def mad(values):
     """Calculates the mad(mean absolute distribution) value from values list"""
     assertList(values)
-    meanValue = mean(values)
-    sum = reduce(lambda a, b: a + abs(b - meanValue), values, 0)
+    u = mean(values)
+    sum = reduce(lambda a, b: a + abs(b - u), values, 0)
     return sum / len(values)
 
 def median(values):
@@ -40,4 +40,68 @@ def range(values):
     """Calculates the range from values list"""
     assertList(values)
     values.sort()
-    return values[len(values) - 1] - values[0]
+    return values[-1] - values[0]
+
+def quarter(values, index):
+    """
+    Calculates the quarter based on the following rule
+    index = 0 -> returns the minimum value
+    index = 1 -> returns the first quarter value
+    index = 2 -> returns the median value
+    index = 3 -> returns the third quarter value
+    index = 4 -> returns the fourth quarter value
+    """
+    assertList(values)
+    values.sort()
+    if (index == 0):
+        return values[0]
+    if (index == 1):
+        if (len(values) % 2 == 0):
+            return median(values[0: len(values) // 2])
+        else:
+            return median(values[0: (len(values) // 2) + 1])
+    if (index == 2):
+        return median(values)
+    if (index == 3):
+        return median(values[len(values) // 2:])
+    if (index == 4):
+        return values[-1]
+
+def iqr(values):
+    """Calculates the IQR from values"""
+    assertList(values)
+    values.sort()
+    return quarter(values, 3) - quarter(values, 1)
+
+def variance(values, n):
+    """Calculates the variance based on the sample and the population quantity n"""
+    assertList(values)
+    if (len(values) <= 1):
+        return values[0]
+    u = mean(values)
+    sum = reduce(lambda a, b: a + abs(b - u) ** 2, values, 0)
+    return sum / n
+
+def variancePopulation(values):
+    """Calculates the variance based on the whole population quantity (n)"""
+    return variance(values, len(values))
+
+def varianceSample(values):
+    """Calculates the variance based on the sample quantity (n - 1)"""
+    return variance(values, len(values) - 1)
+
+def standardDev(values, n):
+    """Calculates the standard deviation on the sample and the population quantity n"""
+    return math.sqrt(variance(values, n))
+
+def standardDevPopulation(values):
+    """Calculates the standard deviation on the whole population quantity (n)"""
+    return math.sqrt(variance(values, len(values)))
+
+def standardDevSample(values):
+    """Calculates the standard deviation on the sample and the population quantity n"""
+    return math.sqrt(variance(values, len(values) - 1))
+
+def zScore(mean, sDev, value):
+    """Calculates the z-score(standard deviation times that a value is from the mean)"""
+    return (value - mean) / sDev
--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -1,4 +1,5 @@
 import pytest
+import math
 # import nose.tools as nt  # contains testing tools like ok_, eq_, etc.
 import mmath.statistics as st
 
@@ -30,4 +31,65 @@ def test_range():
     assert st.range([1, 2, 3]) == 2
     assert st.range([1, 2, 3, 4]) == 3
 
-    
+def test_quarter():
+    assertList()
+    # extremes and median for odds
+    assert st.quarter([1, 2, 3], 0) == 1
+    assert st.quarter([1, 2, 3], 2) == 2
+    assert st.quarter([1, 2, 3], 4) == 3
+    # extremes and median for evens
+    assert st.quarter([1, 2, 3, 4], 0) == 1
+    assert st.quarter([1, 2, 3, 4], 2) == 2.5
+    assert st.quarter([1, 2, 3, 4], 4) == 4
+    # first quartile for odds
+    assert st.quarter([1, 2, 3], 1) == 1.5
+    # first quartile for evens
+    assert st.quarter([1, 2, 3, 4], 1) == 1.5
+    # third quartile for odds
+    assert st.quarter([1, 2, 3], 3) == 2.5
+    # third quartile for evens
+    assert st.quarter([1, 2, 3, 4], 3) == 3.5
+    # single item test
+    assert st.quarter([1], 0) == 1
+    assert st.quarter([1], 1) == 1
+    assert st.quarter([1], 2) == 1
+    assert st.quarter([1], 3) == 1
+    assert st.quarter([1], 4) == 1
+    # two items test
+    assert st.quarter([1, 2], 0) == 1
+    assert st.quarter([1, 2], 1) == 1
+    assert st.quarter([1, 2], 2) == 1.5
+    assert st.quarter([1, 2], 3) == 2
+    assert st.quarter([1, 2], 4) == 2
+
+def test_iqr():
+    assert st.iqr([1, 2, 3]) == 1
+    assert st.iqr([1, 2, 3, 4]) == 2
+    assert st.iqr([1, 2, 3, 4, 5]) == 2
+
+def test_variance():
+    values = [1, 2, 3, 4, 5, 6]
+    assert st.variance(values, len(values)) == 2.9166666666666665
+
+def test_variancePopulation():
+    values = [1, 2, 3, 4, 5, 6]
+    assert st.variancePopulation(values) == 2.9166666666666665
+
+def test_varianceSample():
+    values = [1, 2, 3, 4, 5, 6]
+    assert st.varianceSample(values) == 3.5
+
+def test_standardDev():
+    values = [1, 2, 3, 4, 5, 6]
+    assert st.standardDev(values, len(values)) == math.sqrt(2.9166666666666665)
+
+def test_standardDevPopulation():
+    values = [1, 2, 3, 4, 5, 6]
+    assert st.standardDevPopulation(values) == math.sqrt(2.9166666666666665)
+
+def test_standardDevSample():
+    values = [1, 2, 3, 4, 5, 6]
+    assert st.standardDevSample(values) == math.sqrt(3.5)
+
+def test_zScore():
+    assert st.zScore(60, 15, 70) == 0.6666666666666666
